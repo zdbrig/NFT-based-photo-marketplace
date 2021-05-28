468998b1666edfd8b9db5dfb94c9486a2a8a73a9
amal <fathallahamal86@gmail.com>
2021-05-28 11:43:43 +0200

    update files  project nftmarketplace

--- migrations/5_deploy_PhotoNFTFactory.js
+++ migrations/5_deploy_PhotoNFTFactory.js
@@ -1,10 +1,10 @@
 const PhotoNFTFactory = artifacts.require("./PhotoNFTFactory.sol");
-const PhotoNFTMarketPlace = artifacts.require("./PhotoNFTMarketPlace.sol");
+const PhotoNFTMarketPlace = artifacts.require("./PhotoNFTMarketplace.sol");
 const PhotoNFTData = artifacts.require("./PhotoNFTData.sol");
 
 const _photoNFTMarketPlace = PhotoNFTMarketPlace.address;
 const _photoNFTData = PhotoNFTData.address;
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(PhotoNFTFactory, _photoNFTMarketPlace, _photoNFTData);
 };
