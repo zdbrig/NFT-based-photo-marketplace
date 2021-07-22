d17c3912f80c62522724a2bc9e12496981f778ba
amal <amal@iMac-de-amal.local>
2021-07-22 16:21:55 +0100

    create pages nft

--- nft-borders/src/App.tsx
+++ nft-borders/src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Sigup_1 from "./pages/signup_1"
+import Desktop from "./pages/desktop"
+import WithdrawNFT from "./pages/withdrawnft"
+import RedeemBottle from "./pages/redeembottle"
+import DepositNFT from "./pages/depositnft"
 function App() {
   return (
     <HashRouter>
@@ -12,6 +16,22 @@ function App() {
                         {" "}
                         <Sigup_1Link></Sigup_1Link>{" "}
                     </Route>
+                    <Route exact path="/desktop">
+                        {" "}
+                        <Desktop_Link></Desktop_Link>{" "}
+                    </Route>
+                    <Route exact path="/withdrawNFT">
+                        {" "}
+                        <Withdraw_Link></Withdraw_Link>{" "}
+                    </Route>
+                    <Route exact path="/RedeemBottle">
+                        {" "}
+                        <Redeem_Bottle_Link></Redeem_Bottle_Link>{" "}
+                    </Route>
+                    <Route exact path="/DepositNFT">
+                        {" "}
+                        <DepositNFT_Link></DepositNFT_Link>{" "}
+                    </Route>
                 </Switch>
             </div>
         </HashRouter>
@@ -19,5 +39,17 @@ function App() {
 }
 function Sigup_1Link() {
   return <Sigup_1 />;
+}
+function Desktop_Link() {
+  return <Desktop/>;
+}
+function Withdraw_Link() {
+  return <WithdrawNFT/>;
+}
+function Redeem_Bottle_Link() {
+  return <RedeemBottle/>;
+}
+function DepositNFT_Link() {
+  return <DepositNFT/>;
 }
 export default App;
---
+++ nft-borders/src/pages/desktop.tsx
@@ -0,0 +1,55 @@
+import React ,{ useState, useEffect } from "react";
+import "./desktop.css"
+ import Header from "../component/header/header" 
+function desktop(){
+
+function goWithdraw(){
+    window.location.assign("#/withdrawNFT")
+} 
+function goReedemBottle(){
+    window.location.assign("#/RedeemBottle")
+} 
+function goDepositNFT(){
+    window.location.assign("#/DepositNFT")
+} 
+    return(<div className="Desktop">  
+ <Header></Header>
+
+ <main className="main">
+                       
+                          
+                                <div className="col-12">
+<p className="paraWelcome"> Welcome,</p>
+                                   
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-6 action"> <p className= "paraBottle"> Your bottles</p>
+                                        <div className="bottle">
+                                        <div className="row">
+                                            <div className="col-5"> <img className="imgbottle"src="bottle.png"/></div>
+                                            <div className="col-7">
+                                                <p className="para1837 paramargin">1837</p>
+                                                <p className="paraBottle"> Bottle no.1283</p>
+                                                <p className="paraBottle paraBottleMargin"> View progress details</p>
+                                            </div>
+                                            </div>
+                                        </div>
+                                        
+                                        </div>
+                                        <div className="col-6 action"><p className= "paraActions"> Actions</p>
+                                        <div className="col-12 buttonA" >  <button className="buttonAction" onClick={goWithdraw} > Withdraw NFT</button></div>
+                                        <div className="col-12 buttonA" ><button className="buttonAction" onClick={goReedemBottle}>Redeem Bottle</button> </div>
+                                        <div className="col-12 buttonA"> <button className="buttonAction" onClick={goDepositNFT}>Deposit NFT</button> </div>
+                                        
+                                        </div>
+
+
+                                   
+                                    </div>
+                                    
+                                   
+                                    </main>
+    </div>)
+
+} 
+export default desktop
