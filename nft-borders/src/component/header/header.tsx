40c8934ebd896400d05d75f053d448a377eb8ba0
amal <amal@iMac-de-amal.local>
2021-07-23 09:10:18 +0100

    update project nft

--- nft-borders/src/component/header/header.tsx
+++ nft-borders/src/component/header/header.tsx
@@ -2,7 +2,7 @@ import  "./header.css"
 
 function Header(){
     return(<div className="header">
- <div className="row"> 
+ <div className="col_12"> 
  <div className="col-6 logo"> <p className="paraLogo">Logo</p> </div>
  <div className="col-6 "> <button className="buttonConnect">  Connect</button> </div>
  </div>
--- nft-borders/src/pages/withdrawnft.tsx
+++ nft-borders/src/pages/withdrawnft.tsx
@@ -19,7 +19,7 @@ function withdrawnft(){
 <p className=" parafont paraSteps">Step 2. You require a small amount of Ethereum to pay transaction fees as you withdraw your NFT and use it elsewhere. This guide is helpful: https://help.foundation.app/en/articles/4731452-a-complete-guide-to-getting-eth-and-a-wallet-with-metamask</p>
 <p className="parafont paraSteps">Step 3. Press the “Connect” button in the top right.</p>
 <p className=" parafont paraSteps">Step 4. Press the “Withdraw” button below to then sign to approve the withdrawal</p>
-<button className="buttonWithdraw">Withdraw</button>
+ <div className="container"> <button className="buttonWithdraw">Withdraw</button></div>
 </div>
                                    
                                     </div>
